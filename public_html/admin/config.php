8cccec631fbc3beb6a292ef19ebba4671c12119c
vinh <vinhntps03391@fpt.edu.vn>
2016-08-15 22:06:59 +0700

    vinh

---
+++ public_html/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://ps03391.esy.es/admin/');
+define('HTTP_CATALOG', 'http://ps03391.esy.es/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://ps03391.esy.es/admin/');
+define('HTTPS_CATALOG', 'http://ps03391.esy.es/');
+
+// DIR
+define('DIR_APPLICATION', '/home/u921452033/public_html/admin/');
+define('DIR_SYSTEM', '/home/u921452033/public_html/system/');
+define('DIR_IMAGE', '/home/u921452033/public_html/image/');
+define('DIR_LANGUAGE', '/home/u921452033/public_html/admin/language/');
+define('DIR_TEMPLATE', '/home/u921452033/public_html/admin/view/template/');
+define('DIR_CONFIG', '/home/u921452033/public_html/system/config/');
+define('DIR_CACHE', '/home/u921452033/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/u921452033/public_html/system/storage/download/');
+define('DIR_LOGS', '/home/u921452033/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/u921452033/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home/u921452033/public_html/system/storage/upload/');
+define('DIR_CATALOG', '/home/u921452033/public_html/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'mysql.hostinger.vn');
+define('DB_USERNAME', 'u921452033_vinh');
+define('DB_PASSWORD', 'vinh10061994');
+define('DB_DATABASE', 'u921452033_test');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ public_html/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://ps03391.esy.es/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://ps03391.esy.es/');
+
+// DIR
+define('DIR_APPLICATION', '/home/u921452033/public_html/catalog/');
+define('DIR_SYSTEM', '/home/u921452033/public_html/system/');
+define('DIR_IMAGE', '/home/u921452033/public_html/image/');
+define('DIR_LANGUAGE', '/home/u921452033/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/home/u921452033/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/home/u921452033/public_html/system/config/');
+define('DIR_CACHE', '/home/u921452033/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/u921452033/public_html/system/storage/download/');
+define('DIR_LOGS', '/home/u921452033/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/u921452033/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home/u921452033/public_html/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'mysql.hostinger.vn');
+define('DB_USERNAME', 'u921452033_vinh');
+define('DB_PASSWORD', 'vinh10061994');
+define('DB_DATABASE', 'u921452033_test');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
